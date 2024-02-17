a5b0d542efb718205089a450d13d0af37f90df1e
“rachelphilipose” <rachel@voltaric.com>
2024-02-16 23:09:00 -0500

    i loooove styling <3

--- thehappyhack/src/App.js
+++ thehappyhack/src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/index.scss';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>HELLOO</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className='body'>
+      <h1>Homepage</h1>
+  </div>
   );
-}
+};
 
 export default App;

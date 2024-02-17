816b029ac5551f827846129e3f0846b89d265547
“rachelphilipose” <rachel@voltaric.com>
2024-02-17 01:22:49 -0500

    cards! ui stuff

--- thehappyhack/src/App.js
+++ thehappyhack/src/App.js
@@ -1,11 +1,57 @@
-import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/App.scss';
+import logo from './logo.svg';
 
 const App = () => {
   return (
   <div className='body'>
-      <h1>Homepage</h1>
+
+    <div className='mt-3'>
+      <div className='container'>
+        <h1>theHappyHack ;)</h1>
+      </div>
+    </div>
+
+    <div className='mt-5'>
+      <div className='container'>
+        <h2>Welcome to the Happy Place</h2>
+    <div className='row'>
+      <div className= 'col-sm-4'>
+      <div className="card" >
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+      </div>
+      </div>
+
+
+      
+      <div className= 'col-sm-4'>
+        <div className="card" >
+        <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+        </div>
+        
+
+        <div className= 'col-sm-4'>
+          <div className="card" >
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      </div>
+
+
+    </div>
+
   </div>
   );
 };
